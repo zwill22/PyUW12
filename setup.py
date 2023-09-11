53ba3896feea427ab9dea73b50f357523923181c
Zack Williams <zack.williams@bristol.ac.uk>
2023-09-11 21:45:12 +0100

    Updated setup and added test

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
+import os.path
+
 from setuptools import setup, find_packages
-with open('README.txt') as f:
+
+name = "PyUW12"
+description = "A Python package for UW12 calculations"
+author = "Zack M. Williams"
+author_email = "z.m.williams4@gmail.com"
+url = "https://github.com/zwill22/pyuw12"
+requires_python = ">=3.11.0"
+version = "0.1.0"
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     myLicense = f.read()
 
+setup_location = os.path.abspath(os.path.dirname(__file__))
+
 setup(
-    name="PyUW12",
-    version='0.1.0',
-    description="",
+    name=name,
+    version=version,
+    description=description,
     long_description=readme,
-    author="Zack M. Williams",
-    author_email="z.m.williams4@gmail.com",
-    url="https://github.com/zwill22/pyuw12",
-    packages=find_packages(exclude=('tests', 'docs'))
+    author=author,
+    author_email=author_email,
+    url=url,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=requirements
 )
---
+++ tests/test_pytest.py
@@ -0,0 +1,11 @@
+"""
+Unit test to test pytest functions
+"""
+
+
+def function():
+    return 0
+
+
+def test_function():
+    assert function() == 0
